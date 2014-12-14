caad6400fb47ef02534ef4cb8e1d461ad3866851
Varun Rau <varunrau@gmail.com>
2014-12-13 17:53:52 -0800

    removed object

--- vision.py
+++ vision.py
@@ -20,38 +20,38 @@ class Detector:
             if contour:
                 self.callback(contour)
 
-    def find_largest_contour(self):
-        _, im = self.camera.read()
-        COLOR_MIN = np.array([127, 127, 127],np.uint8)
-        COLOR_MAX = np.array([255, 255, 255],np.uint8)
-        mask = cv2.inRange(im, COLOR_MIN, COLOR_MAX)
-        output = cv2.bitwise_and(im, im, mask = mask)
-        ret,thresh = cv2.threshold(mask,0,255,0)
-        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+def find_largest_contour(self):
+    _, im = self.read()
+    COLOR_MIN = np.array([127, 127, 127],np.uint8)
+    COLOR_MAX = np.array([255, 255, 255],np.uint8)
+    mask = cv2.inRange(im, COLOR_MIN, COLOR_MAX)
+    output = cv2.bitwise_and(im, im, mask = mask)
+    ret,thresh = cv2.threshold(mask,0,255,0)
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-        bounding_rect = None
-        if len(contours) > 0:
-          areas = [cv2.contourArea(c) for c in contours]
-          max_index = np.argmax(areas)
-          cnt=contours[max_index]
+    bounding_rect = None
+    if len(contours) > 0:
+      areas = [cv2.contourArea(c) for c in contours]
+      max_index = np.argmax(areas)
+      cnt=contours[max_index]
 
-          bounding_rect = cv2.boundingRect(cnt)
-          x,y,w,h = bounding_rect
-          cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+      bounding_rect = cv2.boundingRect(cnt)
+      x,y,w,h = bounding_rect
+      cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
 
-        # if self.showCamera:
-        cv2.imshow("images", np.hstack([im, output]))
+    # if self.showCamera:
+    cv2.imshow("images", np.hstack([im, output]))
 
-        return bounding_rect
+    return bounding_rect
 
-# def main():
-#     cap = cv2.VideoCapture(0)
-#     while True:
-#         contour = find_largest_contour(cap)
+def main():
+    cap = cv2.VideoCapture(0)
+    while True:
+        contour = find_largest_contour(cap)
+
+main()
+
+# def hi(stuff):
+#     return ""
 #
-# main()
-
-def hi(stuff):
-    return ""
-
-d = Detector(hi, True)
+# d = Detector(hi, True)
